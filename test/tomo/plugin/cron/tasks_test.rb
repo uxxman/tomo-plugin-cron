0ec86bb16b9b709e1d75e6be02a9c933b92b687f
Muhammad Usman <uxman.sherwani@gmail.com>
2020-11-17 21:18:21 +0100

    Adding task to install and uninstall crontab (#3)

--- test/tomo/plugin/cron/tasks_test.rb
+++ test/tomo/plugin/cron/tasks_test.rb
@@ -1,13 +1,40 @@
 require "test_helper"
 
 class Tomo::Plugin::Cron::TasksTest < Minitest::Test
-  def setup
-    @tester = Tomo::Testing::MockPluginTester.new("cron")
+  def test_show
+    tester = Tomo::Testing::MockPluginTester.new("cron")
+
+    tester.run_task("cron:show")
+
+    assert_equal("crontab -l", tester.executed_script)
   end
 
-  def test_show
-    @tester.run_task("cron:show")
+  def test_install
+    schedule  = "every 1.minute do\n"
+    schedule += "command 'echo hi'\n"
+    schedule += "end"
 
-    assert_equal("crontab -l", @tester.executed_script)
+    temp_file = Tempfile.new
+    temp_file << schedule
+    temp_file.close
+
+    tester = Tomo::Testing::MockPluginTester.new(
+      "cron", settings: { cron_schedule_path: temp_file.path }
+    )
+
+    tester.run_task("cron:install")
+
+    assert_equal(
+      "echo #{'* * * * * /bin/bash -l -c \'echo hi\''.shellescape}'\n''\n' | crontab -",
+      tester.executed_script
+    )
+  end
+
+  def test_uninstall
+    tester = Tomo::Testing::MockPluginTester.new("cron")
+
+    tester.run_task("cron:uninstall")
+
+    assert_equal("crontab -r", tester.executed_script)
   end
 end
